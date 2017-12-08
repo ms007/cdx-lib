c1bf7bf683d0f9f64a13a5087348da5d70825d81
Marc Seiferle <marc.seiferle@swisscom.com>
2017-12-08 18:43:27 +0100

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  external: ['react', 'prop-types'],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins: [
+    resolve({
+      extensions: ['.js', '.jsx']
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
+};
